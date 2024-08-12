7dbc6be4da524e54d056854843b0ec1b87041c0b
UrielBove <ubove@frba.utn.edu.ar>
2024-08-12 00:32:04 -0300

    Sigo

--- programa.pl
+++ programa.pl
@@ -3,50 +3,118 @@
 % Personas Parques y Atracciones
 %Altura esta en centimetros
 
-tiene(nina, 22, 160).
-tiene(marcos, 8, 132).
-tiene(osvaldo, 13, 129).
+tiene(nina, joven, 22, 160).
+tiene(marcos, ninio, 8, 132).
+tiene(osvaldo, adolescente, 13, 129).
 
+persona(nina, 22, 160).
+persona(marcos, 8, 132).
+persona(osvaldo, 13, 129).
 
-atraccion(trenFantasma, Requisito(Edad)):-
-    tiene(Persona, Edad, _),
-    Edad >= 12.
+perteneceAGrupoEtario(Persona, Grupo):-
+    persona(Persona, Edad, _),
+    grupoEtario(Grupo, Min, Max),
+    between(Min, Max, Edad).
+    
 
-atraccion(montaniaRusa, Requisito(Altura)):-
-    tiene(Persona, _, Altura),
-    Altura > 130 .
+grupoEtario(ninio, 0, 12).
+grupoEtario(adolescente, 13, 18).
+grupoEtario(joven, 19, 30).
+grupoEtario(adulto, 31, 100).
 
-atraccion(maquinaTiquetera, Requisito(_)).
+% Parque de la Costa
+
+requisitos(trenFantasma, edad(12)).
+requisitos(montaniaRusa, altura(130)).
+% atraccion(maquinaTiquetera, altura(0)).
 
 %Parque acuatico
 
 % atraccion(NombreAtra, Persona).
 
-atraccion(toboganGigante, Requisito(Altura)):-
-    tiene(Persona, _, Altura),
-    Altura >= 150.
+requisitos(toboganGigante, altura(150)).
+requisitos(piscinaDeOlas, edad(5)).
+% atraccion(rioLento, Requisito(_)).
 
-atraccion(rioLento, Requisito(_)).
 
-atraccion(piscinaDeOlas, Requisito(Edad)):-
-    tiene(Persona, Edad, _),
-    Edad >= 5.
 
 % parque(Atraccion)
 
-parqueDeLaCosta(atraccionNom(trenFantasma, montaniaRusa, maquinaTiquetera), Persona).
-parqueAcuatico(atraccionNom(toboganGigante, rioLento, piscinaDeOlas), Persona).
-
+tieneAtraccion(parqueDeLaCosta, trenFantasma).
+tieneAtraccion(parqueDeLaCosta, montaniaRusa).
+tieneAtraccion(parqueDeLaCosta, maquinaTiquetera).
+tieneAtraccion(parqueAcuatico, toboganGigante).
+tieneAtraccion(parqueAcuatico, piscinaDeOlas).
+tieneAtraccion(parqueAcuatico, rioLento).
 
 % Requerimientos
 
 % Punto 1
 puedeSubir(Persona, Atraccion):-
-    tiene(Persona, _, _),
-    atraccion(Atraccion, Persona).
+    tiene(Persona, _,_, _),
+    tieneAtraccion(_, Atraccion),
+    requisitos(Atraccion, Requisito),
+    cumpleRequisitos(Persona, Requisito).
+puedeSubir(Persona, Atraccion):-
+    tiene(Persona, _,_, _),
+    tieneAtraccion(_, Atraccion),
+    not(requisitos(Atraccion, _)).
+puedeSubir(Persona, Atraccion):-
+    tiene(Persona, _,_, _),
+    tieneAtraccion(_, Atraccion),
+    forall(requisitos(Atraccion, Requisito), cumpleRequisitos(Persona, Requisito)).
+
+cumpleRequisitos(Persona, altura(UnaAltura)):-
+    tiene(Persona, _,_, Altura),
+    Altura >= UnaAltura.
+cumpleRequisitos(Persona, edad(EMinima)):-
+    tiene(Persona, _,Edad, _),
+    Edad >= EMinima.
 
 % Punto 2
 
 esParaElle(Parque, Persona):-
-    tiene(Persona, _, _),
-    forall(Parque(_, Persona), puedeSubir(Persona, _)).
+    tiene(Persona, _,_, _),
+    tieneAtraccion(Parque, _),
+    forall(tieneAtraccion(Parque, Atracciones), puedeSubir(Persona, Atracciones)).
+
+% Punto 3
+
+
+malaIdea(Grupo, Parque):-
+    grupoEtario(Grupo, _, _),
+    tieneAtraccion(Parque, _),
+    forall(tieneAtraccion(Parque, Atracciones), algunoSeLaPierde(Grupo, Atracciones)).
+
+juegoParaTodos(Grupo, Parque):-
+   grupoEtario(Grupo, _, _),
+    tieneAtraccion(Parque, Atraccion),
+    not(algunoSeLaPierde(Grupo, Atraccion)).
+    
+
+algunoSeLaPierde(Grupo, Atraccion):-
+    perteneceAGrupoEtario(Persona, Grupo),
+    not(puedeSubir(Persona, Atraccion)).
+
+% Punto 4 
+
+programaLogico(Programa):-
+    estanEnMismoParque(Programa),
+    noHayRepetidos(Programa).
+
+estanEnMismoParque(Programa):-
+    tieneAtraccion(Parque,Atracciones),
+    forall(member(Atraccion,Programa),member(Atraccion,Atracciones)).
+
+noHayRepetidos([]).
+noHayRepetidos([Cabeza|Cola]):-
+    not(member(Cabeza, Cola)),
+    noHayRepetidos(Cola).
+
+% Alternativa
+programaLogico2(Programa):-
+    estanEnMismoParque(Programa),
+    not(hayRepetidos(Programa)).
+
+hayRepetidos([X|XS]):- member(X,XS).
+hayRepetidos([_|XS]):- hayRepetidos(XS).
